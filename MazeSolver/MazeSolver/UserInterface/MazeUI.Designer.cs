01cc8ee4dc60ef3b19bd3a2021c4544e352fd844
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2022-11-26 19:09:01 +0800

    Replicate MazeSolver from the given example demo

--- MazeSolver/MazeSolver/UserInterface/MazeUI.Designer.cs
+++ MazeSolver/MazeSolver/UserInterface/MazeUI.Designer.cs
@@ -10,7 +10,20 @@ namespace MazeSolver.UserInterface
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private IContainer components = null;
+        private IContainer _componentContainer = null;
+        private PictureBox _mazeContainer;
+        private GroupBox _actionContainer;
+        private CheckBox _allowDiagonalCheckBox;
+
+        private Label _labelPath;
+        private Label _labelPathCaption;
+
+        private RadioButton _drawWallsRadioButton;
+        private RadioButton _findPathRadioButton;
+        
+        private Button _resetButton;
+        private Button _aboutButton;
+        private Button _exitButton;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -18,9 +31,9 @@ namespace MazeSolver.UserInterface
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
+            if (disposing && (_componentContainer != null))
             {
-                components.Dispose();
+                _componentContainer.Dispose();
             }
             base.Dispose(disposing);
         }
@@ -34,12 +47,121 @@ namespace MazeSolver.UserInterface
         private void InitializeComponent()
         {
             this.Text = "Maze Solver";
-            this.components = new Container();
+            this._componentContainer = new Container();
             this.AutoScaleBaseSize = new Size(5, 13);
             this.ClientSize = new Size(608, 381);
+
+            this._mazeContainer = new PictureBox();
+            this._actionContainer = new GroupBox();
+            this._allowDiagonalCheckBox = new CheckBox();
+
+            this._labelPath = new Label();
+            this._labelPathCaption = new Label();
+
+            this._findPathRadioButton = new RadioButton();
+            this._drawWallsRadioButton = new RadioButton();
+            
+            this._resetButton = new Button();
+            this._aboutButton = new Button();
+            this._exitButton = new Button();
+            
+            ((ISupportInitialize) this._mazeContainer).BeginInit();
+            this._actionContainer.SuspendLayout();
+            this.SuspendLayout();
+            
+            this._mazeContainer.BorderStyle = BorderStyle.FixedSingle;
+            this._mazeContainer.Location = new Point(24, 16);
+            this._mazeContainer.Name = "pictureBox1";
+            this._mazeContainer.Size = new Size(403, 336);
+            this._mazeContainer.TabIndex = 0;
+            this._mazeContainer.TabStop = false;
+            this._mazeContainer.MouseMove += new MouseEventHandler(this.OnMazeContainerMouseMove);
+            this._mazeContainer.MouseDown += new MouseEventHandler(this.OnMazeContainerMouseDown);
+            this._mazeContainer.Paint += new PaintEventHandler(this.OnPaintMazeContainer);
+            
+            this._labelPath.Font = new Font("Microsoft Sans Serif", 8.25f, FontStyle.Regular, GraphicsUnit.Point, (byte)0);
+            this._labelPath.Location = new Point(472, 208);
+            this._labelPath.Name = "lblPath";
+            this._labelPath.Size = new Size(128, 16);
+            this._labelPath.TabIndex = 3;
+
+            this._actionContainer.Name = "grpAction";
+            this._actionContainer.Text = "Action";
+            this._actionContainer.Location = new Point(472, 16);
+            this._actionContainer.Size = new Size(120, 104);
+            this._actionContainer.TabIndex = 4;
+            this._actionContainer.TabStop = false;
+            this._actionContainer.Controls.Add((Control) this._findPathRadioButton);
+            this._actionContainer.Controls.Add((Control) this._drawWallsRadioButton);
+
+            this._findPathRadioButton.Location = new Point(8, 64);
+            this._findPathRadioButton.Name = "bFind";
+            this._findPathRadioButton.Size = new Size(104, 24);
+            this._findPathRadioButton.TabIndex = 1;
+            this._findPathRadioButton.Text = "Find Path";
+            this._findPathRadioButton.CheckedChanged += new EventHandler(this.OnCheckChangedFind);
+            
+            this._drawWallsRadioButton.Checked = true;
+            this._drawWallsRadioButton.Location = new Point(8, 24);
+            this._drawWallsRadioButton.Name = "bDraw";
+            this._drawWallsRadioButton.Size = new Size(104, 24);
+            this._drawWallsRadioButton.TabIndex = 0;
+            this._drawWallsRadioButton.TabStop = true;
+            this._drawWallsRadioButton.Text = "Draw Walls";
+            this._drawWallsRadioButton.CheckedChanged += new EventHandler(this.OnCheckChangedDrawWall);
+            
+            this._labelPathCaption.Font = new Font("Microsoft Sans Serif", 9.75f, FontStyle.Bold, GraphicsUnit.Point, (byte)0);
+            this._labelPathCaption.Location = new Point(472, 192);
+            this._labelPathCaption.Name = "lblPathCaption";
+            this._labelPathCaption.Size = new Size(100, 16);
+            this._labelPathCaption.TabIndex = 5;
+            this._labelPathCaption.Text = "Current Path";
+            
+            this._resetButton.Location = new Point(472, 248);
+            this._resetButton.Name = "cmdReset";
+            this._resetButton.Size = new Size(104, 24);
+            this._resetButton.TabIndex = 6;
+            this._resetButton.Text = "&Reset Maze";
+            this._resetButton.Click += new EventHandler(this.OnClickResetButton);
+            
+            this._aboutButton.Location = new Point(472, 288);
+            this._aboutButton.Name = "button1";
+            this._aboutButton.Size = new Size(104, 24);
+            this._aboutButton.TabIndex = 7;
+            this._aboutButton.Text = "&About";
+            this._aboutButton.Click += new EventHandler(this.OnClickAboutButton);
+            
+            this._exitButton.Location = new Point(472, 328);
+            this._exitButton.Name = "cmdExit";
+            this._exitButton.Size = new Size(104, 24);
+            this._exitButton.TabIndex = 8;
+            this._exitButton.Text = "E&xit";
+            this._exitButton.Click += new EventHandler(this.OnClickExitButton);
+            
+            this._allowDiagonalCheckBox.Location = new Point(472, 144);
+            this._allowDiagonalCheckBox.Name = "chkDiagonal";
+            this._allowDiagonalCheckBox.Size = new Size(112, 24);
+            this._allowDiagonalCheckBox.TabIndex = 9;
+            this._allowDiagonalCheckBox.Text = "Allow Diagonals";
+            this._allowDiagonalCheckBox.CheckedChanged += new EventHandler(this.OnCheckChangedAllowDiagonal);
+            
+       
+            this.Controls.Add((Control)this._allowDiagonalCheckBox);
+            this.Controls.Add((Control)this._exitButton);
+            this.Controls.Add((Control)this._aboutButton);
+            this.Controls.Add((Control)this._resetButton);
+            this.Controls.Add((Control)this._labelPathCaption);
+            this.Controls.Add((Control)this._actionContainer);
+            this.Controls.Add((Control)this._labelPath);
+            this.Controls.Add((Control)this._mazeContainer);
+
+            this.Load += new EventHandler(this.OnMazeUILoad);
+            ((ISupportInitialize)this._mazeContainer).EndInit();
+            this._actionContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
         }
 
         #endregion
-
+        
     }
 }
--- MazeSolver/MazeSolver/UserInterface/MazeUI.cs
+++ MazeSolver/MazeSolver/UserInterface/MazeUI.cs
@@ -3,18 +3,154 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MazeSolver.UserInterface
 {
     public partial class MazeUI : Form
     {
+        private int[,] _mazeBoard;
+
+        private int _mazeSize = 20;
+        private int _mazeRowDimensions = 16;
+        private int _mazeColDimensions = 20;
+
+        private int _selectedX;
+        private int _selectedY;
+
         public MazeUI()
         {
             InitializeComponent();
         }
+
+        private void OnMazeUILoad(object sender, EventArgs e)
+        {
+            this.m_Maze = new MazeSolver(this._mazeRowDimensions, this._mazeColDimensions);
+            this._mazeContainer.Size = new Size(this._mazeColDimensions * this._mazeSize + 3, this._mazeRowDimensions * this._mazeSize + 3);
+            this._mazeBoard = this.m_Maze.GetMaze;
+            this._labelPath.Visible = false;
+            this._labelPathCaption.Visible = false;
+        }
+
+        private void OnPaintMazeContainer(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            for (int index1 = 0; index1 < this._mazeRowDimensions; ++index1)
+            {
+                for (int index2 = 0; index2 < this._mazeColDimensions; ++index2)
+                {
+                    graphics.DrawRectangle(new Pen(Color.Black), index2 * this._mazeSize, index1 * this._mazeSize, this._mazeSize, this._mazeSize);
+                    if (this._mazeBoard[index1, index2] == 1)
+                        graphics.FillRectangle((Brush)new SolidBrush(Color.DarkGray), index2 * this._mazeSize + 1, index1 * this._mazeSize + 1, this._mazeSize - 1, this._mazeSize - 1);
+                    if (this._mazeBoard[index1, index2] == 100)
+                        graphics.FillRectangle((Brush)new SolidBrush(Color.Cyan), index2 * this._mazeSize + 1, index1 * this._mazeSize + 1, this._mazeSize - 1, this._mazeSize - 1);
+                }
+            }
+            graphics.FillEllipse((Brush)new SolidBrush(Color.DarkCyan), this._selectedX * this._mazeSize + 5, this._selectedY * this._mazeSize + 5, this._mazeSize - 10, this._mazeSize - 10);
+        }
+
+        private void OnMazeContainerMouseDown(object sender, MouseEventArgs e)
+        {
+            int index1 = e.X / this._mazeSize;
+            int index2 = e.Y / this._mazeSize;
+
+            if (index1 >= this._mazeColDimensions || index1 < 0 || index2 >= this._mazeRowDimensions || index2 < 0)
+                return;
+            if (this._drawWallsRadioButton.Checked)
+            {
+                this._mazeBoard = this.m_Maze.GetMaze;
+                this._mazeBoard[index2, index1] = this._mazeBoard[index2, index1] != 0 ? 0 : 1;
+                this.Refresh();
+            }
+            else if (this._mazeBoard[index2, index1] == 100)
+            {
+                while (this._selectedX != index1 || this._selectedY != index2)
+                {
+                    this._mazeBoard[this._selectedY, this._selectedX] = 0;
+                    if (this._selectedX - 1 >= 0 && this._selectedX - 1 < this._mazeColDimensions && this._mazeBoard[this._selectedY, this._selectedX - 1] == 100)
+                        --this._selectedX;
+                    else if (this._selectedX + 1 >= 0 && this._selectedX + 1 < this._mazeColDimensions && this._mazeBoard[this._selectedY, this._selectedX + 1] == 100)
+                        ++this._selectedX;
+                    else if (this._selectedY - 1 >= 0 && this._selectedY - 1 < this._mazeRowDimensions && this._mazeBoard[this._selectedY - 1, this._selectedX] == 100)
+                        --this._selectedY;
+                    else if (this._selectedY + 1 >= 0 && this._selectedY + 1 < this._mazeRowDimensions && this._mazeBoard[this._selectedY + 1, this._selectedX] == 100)
+                        ++this._selectedY;
+                    else if (this._selectedY + 1 >= 0 && this._selectedY + 1 < this._mazeRowDimensions && this._selectedX + 1 >= 0 && this._selectedX + 1 < this._mazeColDimensions && this._mazeBoard[this._selectedY + 1, this._selectedX + 1] == 100)
+                    {
+                        ++this._selectedX;
+                        ++this._selectedY;
+                    }
+                    else if (this._selectedY - 1 >= 0 && this._selectedY - 1 < this._mazeRowDimensions && this._selectedX + 1 >= 0 && this._selectedX + 1 < this._mazeColDimensions && this._mazeBoard[this._selectedY - 1, this._selectedX + 1] == 100)
+                    {
+                        ++this._selectedX;
+                        --this._selectedY;
+                    }
+                    else if (this._selectedY + 1 >= 0 && this._selectedY + 1 < this._mazeRowDimensions && this._selectedX - 1 >= 0 && this._selectedX - 1 < this._mazeColDimensions && this._mazeBoard[this._selectedY + 1, this._selectedX - 1] == 100)
+                    {
+                        --this._selectedX;
+                        ++this._selectedY;
+                    }
+                    else if (this._selectedY - 1 >= 0 && this._selectedY - 1 < this._mazeRowDimensions && this._selectedX - 1 >= 0 && this._selectedX - 1 < this._mazeColDimensions && this._mazeBoard[this._selectedY - 1, this._selectedX - 1] == 100)
+                    {
+                        --this._selectedX;
+                        --this._selectedY;
+                    }
+                    this.Refresh();
+                }
+            }
+        }
+
+        private void OnMazeContainerMouseMove(object sender, MouseEventArgs e)
+        {
+            int iToY = e.Y / this._mazeSize;
+            int iToX = e.X / this._mazeSize;
+            if (iToX >= this._mazeColDimensions || iToX < 0 || iToY >= this._mazeRowDimensions || iToY < 0)
+                return;
+            this._mazeBoard = this.m_Maze.GetMaze;
+            if (!this._drawWallsRadioButton.Checked)
+            {
+                int[,] path = this.m_Maze.FindPath(this._selectedY, this._selectedX, iToY, iToX);
+                if (path != null)
+                {
+                    this._mazeBoard = path;
+                    this._labelPath.Text = "" + (object)this._selectedY + "," + (object)this._selectedX + " to " + (object)iToY + "," + (object)iToX;
+                }
+                else
+                    this._labelPath.Text = "No Path Found";
+                this.Refresh();
+            }
+        }
+
+        private void OnCheckChangedDrawWall(object sender, EventArgs e)
+        {
+            this._mazeBoard = this.m_Maze.GetMaze;
+            this._labelPath.Visible = false;
+            this._labelPathCaption.Visible = false;
+            this.Refresh();
+        }
+
+        private void OnCheckChangedFind(object sender, EventArgs e)
+        {
+            this.m_Maze.AllowDiagonals = this._allowDiagonalCheckBox.Checked;
+            this._labelPath.Visible = true;
+            this._labelPathCaption.Visible = true;
+        }
+
+        private void OnClickResetButton(object sender, EventArgs e)
+        {
+            this.m_Maze = new MazeSolver(this._mazeRowDimensions, this._mazeColDimensions);
+            this.m_Maze.AllowDiagonals = this._allowDiagonalCheckBox.Checked;
+            this._mazeBoard = this.m_Maze.GetMaze;
+            this.Refresh();
+        }
+
+        private void OnClickAboutButton(object sender, EventArgs e)
+        {
+            int num = (int)MessageBox.Show(" Maze Solver Demo");
+        }
+
+        private void OnClickExitButton(object sender, EventArgs e) => this.Close();
+
+        private void OnCheckChangedAllowDiagonal(object sender, EventArgs e) => this.m_Maze.AllowDiagonals = this._allowDiagonalCheckBox.Checked;
     }
 }
